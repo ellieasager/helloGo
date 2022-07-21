46adbaf8d9afc963517bf0aa731380dd5b2aa4ef
Ellie A Sager <ellie@firstfoundry.co>
2022-07-21 14:48:55 -0700

    stuff

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ellieasager/helloGo
+
+go 1.18
+
+require github.com/ellieasager/go v0.0.0-20220720214804-720d13d5625d // indirect
---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// import "github.com/ellieasager/helloGo/stringsFun"
+
+func main() {
+	fmt.Println("Hello, world.")
+	// fmt.Println(stringsFun.ReverseRunes("!oG ,olleH"))
+}
